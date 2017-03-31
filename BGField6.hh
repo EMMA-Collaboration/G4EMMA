93d3f83fdf0857a855f383dc9a4c9dfb64d01c66
g4emmaStudent <g4emmaStudent@users.noreply.github.com>
2017-03-31 13:54:29 -0700

    Add files via upload
    This is a RE-SUBMISSION of BGField5, along with BGField6. Ignore the previous commit of BGField5.

--- BGField6.hh
+++ BGField6.hh
@@ -1,10 +1,10 @@
 //!
 //! Created from BuildGeometry program
 //!
-#ifndef BGField5_H
-#define BGField5_H 1
+#ifndef BGField6_H
+#define BGField6_H 1
 #include "globals.hh"
-#include "G4ElectricField.hh"
+#include "G4MagneticField.hh"
 
 #include "G4LogicalVolume.hh"
 #include "G4ThreeVector.hh"
@@ -18,19 +18,21 @@ extern G4double currentCharge; //! EMMASteppingAction.cc
 extern G4double userCharge; //! EMMAPrimaryGeneratorAction.cc
 
 
-//class BGField5 : public G4ElectricField
-class BGField5 : public EMMAElementField
+//class BGField6 : public G4MagneticField
+class BGField6 : public EMMAElementField
+
 {
 public:
   //! Constructor
   //! offset coordinates are of the x and z location of where the field begins in reference to 
   //! the world logical volume
-  //! BGField5(G4double xoffset, G4double zoffset,G4double zbefore,G4double zafter);
- BGField5(G4double xoffset, G4double zoffset,G4double zbefore,G4double zafter, G4LogicalVolume*, G4ThreeVector);
-  ~BGField5();
+  //! BGField6(G4double xoffset, G4double zoffset,G4double zbefore,G4double zafter);
+   BGField6(G4double xoffset, G4double zoffset,G4double zbefore,G4double zafter, G4LogicalVolume*, G4ThreeVector);
+  ~BGField6();
 
   //! instead you can limit the size
   //! Retrieves length, width, and height (of type G4double) and returns the maximum representable finite double number
+  
   virtual G4double GetLength() { return DBL_MAX; }
   virtual G4double GetWidth() { return DBL_MAX; }
   virtual G4double GetHeight() { return DBL_MAX; }
@@ -43,23 +45,22 @@ public:
   //! declares a 75-element array of doubles named "data"
   //! and a 3-element array of doubles named "offset"
   //! declares variables i (of type G4int) and Pi (of type G4double)
-  //! declares variable FieldStrength_0 (of type G4double)
-  
+  //! declares variable FieldStrength_0 (of type G4double)  
+ 
   
 private:
   double data[75], offset[3];
   G4int i;
   G4double Pi;
   G4double FieldStrength_0;
-
-  //! GetFieldStrength() returns data[14]
-  //! ScaleFieldStrength(G4double) sets data[14] to the
+  
+  //! GetFieldStrength() returns data[13]
+  //! ScaleFieldStrength(G4double) sets data[13] to the
   //! product of the G4double parameter and FieldStrength_0
-  //! WHY IS THE ScaleFieldStrength METHOD NOT THE SAME AS THE ONE IN BGFIELD3???
 
 public:
-  G4double GetFieldStrength() { return data[14]; }; 
-  void ScaleFieldStrength( G4double esf ) { data[14] = esf * FieldStrength_0 ; }; 
+  G4double GetFieldStrength() { return data[13]; }; 
+  void ScaleFieldStrength( G4double msf ) { data[13] = msf * FieldStrength_0 ; }; 
 
 };
 #endif
